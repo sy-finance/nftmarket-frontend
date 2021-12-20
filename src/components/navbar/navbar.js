c599c1c2adbc761544ed837b4e7d37bdab187986
sy-finance <syfinance@protonmail.com>
2021-12-20 00:04:23 -0700

    Add Github link to nav

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
 
+import Img from 'react-cool-img';
+
 import {Navbar, Nav, NavItem, NavDropdown, DropdownButton, MenuItem, CollapsibleNav} from 'react-bootstrap';
 
 import SyfinAvatars from '../../abis/SyfinAvatars.json';
@@ -329,7 +331,7 @@ class NavbarMain extends Component {
                       /></Nav.Link>
                     <NavDropdown title={
                         (this.state.ipfs !== "" && (this.state.mim === "image/jpeg" || this.state.mim === "image/png" || this.state.mim === "image/gif")) ?
-                        ( <div style={{position: "relative", width: "38px", margin: "0 auto"}}><img src={"https://ipfs.sy.finance/ipfs/"+this.state.ipfs} alt="" border="0" height="38px" width="38px" style={{borderRadius: "50%"}} />
+                        ( <div style={{position: "relative", width: "38px", margin: "0 auto"}}><Img src={"https://ipfs.sy.finance/ipfs/"+this.state.ipfs} cache alt="" border="0" height="38px" width="38px" style={{borderRadius: "50%"}} />
                         
                         {(this.state.verified === true) ? (
                         <div style={{position: "absolute", bottom: "-2px", right: "-1px"}}><svg width="14" height="14" viewBox="0 0 12 12" fill="#4E78FF" xmlns="http://www.w3.org/2000/svg"><path d="M4.78117 0.743103C5.29164 -0.247701 6.70826 -0.247701 7.21872 0.743103C7.52545 1.33846 8.21742 1.62509 8.8553 1.42099C9.91685 1.08134 10.9186 2.08304 10.5789 3.1446C10.3748 3.78247 10.6614 4.47445 11.2568 4.78117C12.2476 5.29164 12.2476 6.70826 11.2568 7.21872C10.6614 7.52545 10.3748 8.21742 10.5789 8.8553C10.9186 9.91685 9.91685 10.9186 8.8553 10.5789C8.21742 10.3748 7.52545 10.6614 7.21872 11.2568C6.70826 12.2476 5.29164 12.2476 4.78117 11.2568C4.47445 10.6614 3.78247 10.3748 3.1446 10.5789C2.08304 10.9186 1.08134 9.91685 1.42099 8.8553C1.62509 8.21742 1.33846 7.52545 0.743103 7.21872C-0.247701 6.70826 -0.247701 5.29164 0.743103 4.78117C1.33846 4.47445 1.62509 3.78247 1.42099 3.1446C1.08134 2.08304 2.08304 1.08134 3.1446 1.42099C3.78247 1.62509 4.47445 1.33846 4.78117 0.743103Z" fill="#FFF"></path><path fillRule="evenodd" clipRule="evenodd" d="M8.43961 4.23998C8.64623 4.43922 8.65221 4.76823 8.45297 4.97484L5.40604 8.13462L3.54703 6.20676C3.34779 6.00014 3.35377 5.67113 3.56039 5.47189C3.76701 5.27266 4.09602 5.27864 4.29526 5.48525L5.40604 6.63718L7.70475 4.25334C7.90398 4.04672 8.23299 4.04074 8.43961 4.23998Z" fill="#000"></path></svg></div>
@@ -364,6 +366,7 @@ class NavbarMain extends Component {
                       <NavDropdown.Item href={"https://info.wraithswap.finance"} target="_blank">Analytics 🡕</NavDropdown.Item>
                       <NavDropdown.Item href={"https://vote.wraithswap.finance/#/wraithswap.eth"} target="_blank">Voting 🡕</NavDropdown.Item>
                       <NavDropdown.Item href={"https://syfinance.gitbook.io/sy-finance/"} target="_blank">Documentation 🡕</NavDropdown.Item>
+                      <NavDropdown.Item href={"https://github.com/sy-finance/nftmarket-frontend"} target="_blank">Github 🡕</NavDropdown.Item>
                     </NavDropdown>
                     </Nav>
                         </>
